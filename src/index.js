433b8f7b641ad35a3b63880dba65ee439caa1018
Mayza Ferreira <64496134+mayza-ferreira@users.noreply.github.com>
2025-07-04 15:57:27 -0300

    feat: Update recipe input form hints and enhance styling for better user experience

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function displayRecipe(response) {
   new Typewriter("#recipe-text", {
-    strings: response.data.answer,
+    strings: response.data.answer.replace(/\n/g, "<br>"),
     autoStart: true,
     cursor: null,
     delay: 10,
@@ -13,7 +13,7 @@ function generateRecipe(event) {
   let apiKey = "b3ec35b0cd4d44ft6c3d0oaf1862e31d";
   let prompt = document.getElementById("user-instructions").value;
   let context =
-    "You are a helpful assistant. You can provide explanations, and assist only with various cooking recipes. Please respond with a formated recipe, an ingredient list with bullet points, and the step by step, lastly some tips. Respond in the language of the prompt.";
+    "You are a helpful assistant. You can provide explanations, and assist only with various cooking recipes. Please respond with a formated recipe, an ingredient list, and the step by step, lastly some tips. Respond in the language of the prompt.Use emojis to make it more engaging. Do not include any other information.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeText = document.getElementById("recipe-text");
